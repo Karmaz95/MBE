930bad4f6f65bfe427027809dcc44096223773d1
karmaz95 <karol.mazurek95@gmail.com>
2021-04-18 18:15:19 +0200

    Lab2 - solution

--- lab1/cracker_lab2.c
+++ lab1/cracker_lab2.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
-//cracker for LAB1
+//cracker for LAB2
 int main()
 {
-    char *char_ptr, pass[] = "5tr0vZBrX:xTyR-P!"; 
+    char *char_ptr, pass[] = "kw6PZq3Zd;ekR[_1"; 
     int pass_length = 17;
 
     for(int i = 0; i < pass_length; ++i)
     {
         char_ptr = &pass[i];
-    	printf("%c", (*char_ptr ^ i));
+    	printf("%c", (*char_ptr ^ (i+1)));
     }
     
     return 0;
